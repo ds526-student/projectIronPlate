13a79c9f484dcb0d0206a10d7d585ccab116ef24
ds526-student <damionsklenarsclare@gmail.com>
2025-08-11 14:08:22 +1200

    internal food navigation

--- src/app/food/_layout.tsx
+++ src/app/food/_layout.tsx
@@ -1,9 +1,14 @@
-import { Stack } from "expo-router";
+import { Stack, usePathname } from "expo-router";
 
 export default function Layout(){
+    const pathname = usePathname();
+
     return(
-        <Stack>
+        <Stack screenOptions={{ animation: "none" }}>
             <Stack.Screen name="index" options={{ title: "Food"}} />
+            <Stack.Screen name="addFood" options={{ title: "Add Food"}} />
+            <Stack.Screen name="createRecipe" options={{ title: "Create Recipe"}} />
+            <Stack.Screen name="barcodeScan" options={{ title: "Scan Barcode"}} />
         </Stack>
     );
 }
--- src/app/food/index.tsx
+++ src/app/food/index.tsx
@@ -1,11 +1,46 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { commonStyles } from '../../styles/commonStyles';
+import { colors } from '../../styles/colors';
+import { Link } from 'expo-router';
 
 export default function FoodScreen() {
     return(
         <View style={{flex: 1, justifyContent: 'center', padding: 4}}>
-            <Text style={commonStyles.centerText}>Food</Text>
+            <View style={[commonStyles.container, {flex: 3, backgroundColor: colors.primary}]}>
+                <Text style={[commonStyles.title, commonStyles.centerText]}>Stats Section</Text>
+            </View>
+            <View style={[commonStyles.container, {flex: 4, backgroundColor: colors.accent}]}>
+                <View style={{flex: 2, flexDirection: 'row'}}>
+                    <View style={{flex: 1, flexDirection: 'column'}}>
+                        <Link href="/food/addFood" style={{flex: 1, margin: 1, backgroundColor: 'blue'}}>
+                            <View>
+                                <Text style={[commonStyles.title, commonStyles.centerText]}>+ {'\n'} Add Food</Text>
+                            </View>
+                        </Link>
+                        <Link href="/food/createRecipe" style={{flex: 1, margin: 1, backgroundColor: 'green'}}>
+                            <View>
+                                <Text style={[commonStyles.title, commonStyles.centerText]}>Create Recipe</Text>
+                            </View>
+                        </Link>
+                    </View>
+                    <View style={{flex: 1, flexDirection: 'column'}}>
+                        <Link href="/food/barcodeScan" style={{flex: 1, margin: 1, backgroundColor: 'red'}}>
+                            <View>
+                                <Text style={[commonStyles.title, commonStyles.centerText]}>Scan Barcode</Text>
+                            </View>
+                        </Link>
+                        <Link href="/food/addFavFood" style={{flex: 1, margin: 1, backgroundColor: 'purple'}}>
+                            <View>
+                                <Text style={[commonStyles.title, commonStyles.centerText]}>Add from Favourites</Text>
+                            </View>
+                        </Link>
+                    </View>
+                </View>
+            </View>
+            <View style={[commonStyles.container, {flex: 6, backgroundColor: colors.primary}]}>
+                <Text style={commonStyles.title}>Skinny Bitch go Eat Something</Text>
+            </View>
         </View>
     );  
 }
