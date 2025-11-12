d49a11b1bb1b5a1e32c2e232dabb57a2deda3986
ds526-student <damionsklenarsclare@gmail.com>
2025-11-13 00:34:39 +1300

    calorie card created
    created a card that displays calories remaining and a circular progress view

--- app/src/main/java/com/example/projectironplate/ui/food/FoodFragment.java
+++ app/src/main/java/com/example/projectironplate/ui/food/FoodFragment.java
@@ -1,16 +1,20 @@
 package com.example.projectironplate.ui.food;
 
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.projectironplate.R;
 import com.example.projectironplate.databinding.FragmentFoodBinding;
+import com.google.android.material.progressindicator.CircularProgressIndicator;
 
 public class FoodFragment extends Fragment {
 
@@ -24,9 +28,27 @@ public class FoodFragment extends Fragment {
         binding = FragmentFoodBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textFood;
-        foodViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        setUpCalorieCounter(root, 100, 200);
+
         return root;
+    }
+
+    private void setUpCalorieCounter(View root, int remainingCals, int goalCals) {
+        LinearLayout container = root.findViewById(R.id.food_container);
+
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View calorieCard = inflater.inflate(R.layout.card_food_calories, container, false);
+
+        float percentF = (goalCals > 0) ? (remainingCals * 100f) / goalCals : 0f;
+        int percent = Math.round(Math.max(0f, Math.min(100f, percentF)));
+
+        TextView remainingCalsText = calorieCard.findViewById(R.id.tv_remaining_cals);
+        remainingCalsText.setText(String.valueOf(remainingCals));
+
+        CircularProgressIndicator progress = calorieCard.findViewById(R.id.calorie_progress);
+        progress.setProgress(percent);
+
+        container.addView(calorieCard);
     }
 
     @Override
--- app/src/main/java/com/example/projectironplate/ui/workouts/WorkoutsFragment.java
+++ app/src/main/java/com/example/projectironplate/ui/workouts/WorkoutsFragment.java
@@ -24,8 +24,6 @@ public class WorkoutsFragment extends Fragment {
         binding = FragmentWorkoutsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textWorkouts;
-        workoutsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
